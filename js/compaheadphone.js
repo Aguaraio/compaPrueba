f0486ef3894e2afba577eeea4e096b5fad778b9d
xavilopez <xavi_lopez011@hotmail.com>
2017-01-23 14:28:05 -0300

    asd

--- js/compaheadphone.js
+++ js/compaheadphone.js
@@ -239,7 +239,7 @@ $(function() {
     generatedDiv = '<div class="imodalItem" id="modalItem'+ idTile +'">'+
                     '<div class="demo-card-square mdl-cardiModal mdl-shadow--2dp">'+
                       '<div class="card-title">'+
-                        '<div>'+ table[idTile][3] +'</div>'+  /*IMAGE*/
+                        '<div class="iModalImg">'+ table[idTile][3] +'</div>'+  /*IMAGE*/
                         '<div class="btnClose">'+
                           '<span class="glyphicon glyphicon-remove"></span>'+
                         '</div>'+
